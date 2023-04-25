19f59982dd5d05b4f00fda76fc4845e377708f48
unknown <gosgie02@wp.pl>
2023-04-25 20:47:45 +0200

    poprawiono dzialanie konstruktora w przypadku podania niewszystkich argumentow oraz metody ToString()

--- pudelko/Program.cs
+++ pudelko/Program.cs
@@ -1,8 +1,4 @@
 ﻿using pudelko;
 using System.Globalization;
-CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
-Console.WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
-Pudelko p1 = new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter);
-//Pudelko p2 = Pudelko.Parse("2.500 m × 9.321 m × 0.100 m");
-Console.WriteLine(p1.ToString("cm"));
-//Console.WriteLine(p2.ToString("cm"));
+Pudelko p = new Pudelko(unit: UnitOfMeasure.centimeter, a: 11.0);
+Console.WriteLine(p);
--- pudelko/Pudelko.cs
+++ pudelko/Pudelko.cs
@@ -43,33 +43,36 @@ namespace pudelko
             }
         }
         
-        public Pudelko(double a = 0.1, double b = 0.1, double c = 0.1, UnitOfMeasure unit = UnitOfMeasure.meter)
+        public Pudelko(double a = 10, double b = 10, double c = 10, UnitOfMeasure unit = UnitOfMeasure.meter)// domyslnie 10 cm
         {
             this.unit = unit;
             
             if (unit == UnitOfMeasure.centimeter)
             {
-                if (this.a == default) this.a = a * 100;
-                if (this.b == default) this.b = b * 100;
-                if (this.c == default) this.c = c * 100;
-
-                if (a > 1000 || b > 1000 || c > 1000) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                //if (a == default) this.a = a*100; else this.a = a;
+                //if (b == default) this.b = b*100; else this.b = b;
+                //if (c == default) this.c = c*100; else this.c = c;
+                //if (a > 1000 || b > 1000 || c > 1000) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
             }
             if (unit == UnitOfMeasure.milimeter)
             {
-                if (this.a == default) this.a = a * 1000;
-                if (this.b == default) this.b = b * 1000;
-                if (this.c == default) this.c = c * 1000;
-
-                if (a > 10000 || b > 10000 || c > 10000) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
+                if (a == default) this.a = a * 100; else this.a = a;
+                if (b == default) this.b = b * 100; else this.b = b;
+                if (c == default) this.c = c * 100; else this.c = c;
+                //if (a > 10000 || b > 10000 || c > 10000) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
             }
-
-            this.a = a;
-            this.b = b;
-            this.c = c;
-
-            if (a > 10 || b > 10 || c > 10) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
-            if (a <= 0 || b <= 0 || c <= 0) throw new ArgumentOutOfRangeException("Długość krawędzi musi być dodatnia");
+            if(unit == UnitOfMeasure.meter)
+            {
+                if (a == default) this.a = a / 100; else this.a = a;
+                if (b == default) this.b = b / 100; else this.b = b;
+                if (c == default) this.c = c / 100; else this.c = c;
+            }
+            
+            if (A > 10 || B > 10 || C > 10) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
+            if (A <= 0 || B <= 0 || C <= 0) throw new ArgumentOutOfRangeException("Długość krawędzi musi być dodatnia");
         }
         // zwraca objętość pudełka w m^3 w zaokrągleniu do 9 miejsc po przecinku
         public double Objetosc => Math.Round(A * B * C, 9);
@@ -84,10 +87,10 @@ namespace pudelko
         }
         public override string ToString()
         {
-            if (unit == UnitOfMeasure.centimeter)
-                return $"{Math.Round(A / 100, 3)} m × {Math.Round(B / 100, 3)} m × {Math.Round(C / 100, 3)} m";
-            if (unit == UnitOfMeasure.milimeter)
-                return $"{Math.Round(A / 1000, 3)} m × {Math.Round(B / 1000, 3)} m × {Math.Round(C / 1000, 3)} m";
+            //if (this.unit == UnitOfMeasure.centimeter)
+            //    return $"{Math.Round(A / 100, 3)} m × {Math.Round(B / 100, 3)} m × {Math.Round(C / 100, 3)} m";
+            //if (this.unit == UnitOfMeasure.milimeter)
+            //    return $"{Math.Round(A / 1000, 3)} m × {Math.Round(B / 1000, 3)} m × {Math.Round(C / 1000, 3)} m";
             return $"{Math.Round(A, 3)} m × {Math.Round(B, 3)} m × {Math.Round(C, 3)} m";
         }
 
@@ -97,9 +100,9 @@ namespace pudelko
             if (fmt == "m")
                 return $"{Math.Round(A, 3)} m × {Math.Round(B, 3)} m × {Math.Round(C, 3)} m";
             if (fmt == "cm")
-                return $"{Math.Round(A * 100, 3)} cm × {Math.Round(B * 100, 3)} cm × {Math.Round(C * 100, 3)} cm";
+                return $"{Math.Round(A * 100, 1)} cm × {Math.Round(B * 100, 1)} cm × {Math.Round(C * 100, 1)} cm";
             if (fmt == "mm")
-                return $"{Math.Round(A * 1000, 3)} mm × {Math.Round(B * 1000, 3)} mm × {Math.Round(C * 1000, 3)} mm";
+                return $"{Math.Round(A * 1000, 0)} mm × {Math.Round(B * 1000, 0)} mm × {Math.Round(C * 1000, 0)} mm";
             throw new FormatException();
         }
 
@@ -193,7 +196,9 @@ namespace pudelko
 
         public static implicit operator Pudelko(ValueTuple<int, int, int> tuple)
         {
-            return new Pudelko(tuple.Item1, tuple.Item2, tuple.Item3);
+            if (tuple.Item1 <= 0 || tuple.Item2 <= 0 || tuple.Item3 <= 0) throw new ArgumentOutOfRangeException("Długość krawędzi musi być dodatnia");
+            if (tuple.Item1 > 10 || tuple.Item2 > 10 || tuple.Item3 > 10) throw new ArgumentOutOfRangeException("Długość krawędzi nie może przekrozyć 10m");
+            return new Pudelko(tuple.Item1, tuple.Item2, tuple.Item3, UnitOfMeasure.milimeter);
         }
 
         public double this[int index]
